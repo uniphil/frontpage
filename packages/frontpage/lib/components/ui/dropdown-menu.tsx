4ee97d64bdbc74810e07758f81bf8a6d5b7080f8
Jake Saterlay <41985955+JakeSaterlay@users.noreply.github.com>
2024-11-06 20:51:43 +0000

    changed cursor to pointer on theme options (#198)
    * changed cursor to pointer on theme options
    
    * removed class from theme-toggle and added to base component

--- packages/frontpage/lib/components/ui/dropdown-menu.tsx
+++ packages/frontpage/lib/components/ui/dropdown-menu.tsx
@@ -140,7 +140,7 @@ function DropdownMenuRadioItem({
   return (
     <DropdownMenuPrimitive.RadioItem
       className={cn(
-        "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        "relative flex cursor-pointer select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
         className,
       )}
       {...props}
