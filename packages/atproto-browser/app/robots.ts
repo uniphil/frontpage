4dab0a8d230622c69b9ced0ed20fbfc893a77a50
Tom Sherman <the.tomsherman@gmail.com>
2024-11-28 17:47:42 +0000

    Allow indexing home page

--- packages/atproto-browser/app/robots.ts
+++ packages/atproto-browser/app/robots.ts
@@ -4,6 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      // Disallow all paths except for the root path.
+      allow: "/$",
       disallow: "/",
     },
   };
