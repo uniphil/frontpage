313ae6237ba3a10e8bc00b08ef032e21ba37d7b1
Tom Sherman <the.tomsherman@gmail.com>
2025-01-15 11:57:53 +0000

    Show invalid record when com.atproto.repo.getRecord returns a different value to com.atproto.sync.getRecord

--- packages/atproto-browser/app/at/[identifier]/[collection]/[rkey]/page.tsx
+++ packages/atproto-browser/app/at/[identifier]/[collection]/[rkey]/page.tsx
@@ -2,7 +2,8 @@ import { JSONType, JSONValue } from "@/app/at/_lib/atproto-json";
 import { resolveIdentity } from "@/lib/atproto-server";
 import { getHandle, getKey, getPds } from "@atproto/identity";
 import { verifyRecords } from "@atproto/repo";
-import { Suspense } from "react";
+import { cache, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function RkeyPage(props: {
   params: Promise<{
@@ -26,28 +27,15 @@ export default async function RkeyPage(props: {
     return <div>🚨 No PDS found for DID: {didDocument.id}</div>;
   }
 
-  const getRecordUrl = new URL(`${pds}/xrpc/com.atproto.repo.getRecord`);
-  getRecordUrl.searchParams.set("repo", didDocument.id);
-  getRecordUrl.searchParams.set("collection", params.collection);
-  getRecordUrl.searchParams.set("rkey", params.rkey);
+  const getRecordResult = await getRecord(
+    didDocument.id,
+    params.collection,
+    params.rkey,
+  );
 
-  const response = await fetch(getRecordUrl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!response.ok) {
-    return (
-      <div>
-        🚨 Failed to fetch record: {response.statusText}. URL:{" "}
-        {getRecordUrl.toString()}
-      </div>
-    );
+  if (!getRecordResult.success) {
+    return <div>🚨 Failed to fetch record: {getRecordResult.error}</div>;
   }
-
-  const record = (await response.json()) as JSONType;
 
   return (
     <>
@@ -56,22 +44,26 @@ export default async function RkeyPage(props: {
         href={`at://${identityResult.didDocument.id}/${params.collection}/${params.rkey}`}
       />
       <h2>
-        Record
-        <Suspense
-          fallback={
-            <span title="Verifying record..." aria-busy>
-              🤔
-            </span>
-          }
+        Record{" "}
+        <ErrorBoundary
+          fallback={<span title="Error verifying record.">🤔</span>}
         >
-          <RecordVerificationBadge
-            did={didDocument.id}
-            collection={params.collection}
-            rkey={params.rkey}
-          />
-        </Suspense>
+          <Suspense
+            fallback={
+              <span title="Verifying record..." aria-busy>
+                🤔
+              </span>
+            }
+          >
+            <RecordVerificationBadge
+              did={didDocument.id}
+              collection={params.collection}
+              rkey={params.rkey}
+            />
+          </Suspense>
+        </ErrorBoundary>
       </h2>
-      <JSONValue data={record} repo={didDocument.id} />
+      <JSONValue data={getRecordResult.record} repo={didDocument.id} />
     </>
   );
 }
@@ -113,15 +105,39 @@ async function RecordVerificationBadge({
       </span>
     );
   }
-  const car = new Uint8Array(await response.arrayBuffer());
+  const proofBytes = new Uint8Array(await response.arrayBuffer());
   const key = getKey(didDoc);
   if (!key) {
     return <span title="Invalid record (no key)">❌</span>;
   }
 
+  // No need to worry about sequential requests here, should have already been fetched and in the cache
+  const recordResult = await getRecord(did, collection, rkey);
+  if (!recordResult.success) {
+    throw new Error(recordResult.error);
+  }
+
   try {
-    await verifyRecords(car, did, key);
-    return <span title="Valid record">🔒</span>;
+    const [claim] = await verifyRecords(proofBytes, did, key);
+    if (!claim || !claim.record) {
+      return <span title="Invalid record (no record)">❌</span>;
+    }
+
+    // Whether we compare bytes or JSON or object value (ie. key order independent) isn't specced out. It's easiest for us to compare JSON so we'll do that.
+    // Bytes would be the most strict equality check while object value would be the least strict, JSON is somewhere in the middle in terms of strictness as it's sensitive to different key ordering.
+    if (
+      JSON.stringify(claim.record) !==
+      JSON.stringify(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (recordResult.record as any)?.value,
+      )
+    ) {
+      return (
+        <div style={{ display: "inline-block" }}>
+          <span title="Invalid record (mismatch)">❌</span>
+        </div>
+      );
+    }
   } catch (e) {
     if (e instanceof Error) {
       return <span title={`Invalid record (${e.message})`}>❌</span>;
@@ -129,4 +145,63 @@ async function RecordVerificationBadge({
       return <span title="Invalid record (unknown)">❌</span>;
     }
   }
+
+  return <span title="Valid record">🔒</span>;
 }
+
+type GetRecordResult =
+  | {
+      success: true;
+      record: JSONType;
+    }
+  | {
+      success: false;
+      error: string;
+    };
+
+/**
+ * Errors thrown in this function shuould be invariants that are checked before the function is called.
+ */
+const getRecord = cache(
+  async (
+    did: string,
+    collection: string,
+    rkey: string,
+  ): Promise<GetRecordResult> => {
+    const identityResult = await resolveIdentity(did);
+    if (!identityResult.success) {
+      throw new Error(identityResult.error);
+    }
+
+    const didDocument = identityResult.didDocument;
+
+    const pds = getPds(didDocument);
+    if (!pds) {
+      throw new Error("No PDS found for DID");
+    }
+
+    const getRecordUrl = new URL(`${pds}/xrpc/com.atproto.repo.getRecord`);
+    getRecordUrl.searchParams.set("repo", didDocument.id);
+    getRecordUrl.searchParams.set("collection", collection);
+    getRecordUrl.searchParams.set("rkey", rkey);
+
+    const response = await fetch(getRecordUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      return {
+        success: false as const,
+        error: `${response.statusText}. URL: ${getRecordUrl.toString()}`,
+      };
+    }
+
+    return {
+      success: true as const,
+      record: (await response.json()) as JSONType,
+    };
+  },
+);
